15f2e67b66329a6ad8080a7e568a013b3567d6d3
PiyushGaywal <142382705+PiyushGaywal@users.noreply.github.com>
2025-04-21 19:24:06 +0530

    Fixed Render publish directory and start script

--- Utils/db.js
+++ Utils/db.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 
-const Url="mongodb://localhost:27017/Hotelb"
+const Url="mongodb+srv://Piyu1cbz:Piyu1cbz@mydatabase.bvhet.mongodb.net/?retryWrites=true&w=majority&appName=MyDatabase"
 
 const db=mongoose.connect(Url).
 then(()=>{console.log('Connect To Database Success');
