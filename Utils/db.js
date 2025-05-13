1a8a3fee2a835691d03f9217baea589c721adf73
PiyushGaywal <142382705+PiyushGaywal@users.noreply.github.com>
2025-05-13 19:40:18 +0530

    Small Change in css

--- Utils/db.js
+++ Utils/db.js
@@ -1,12 +1,8 @@
 const mongoose=require('mongoose')
-
-const Url="mongodb+srv://Piyu1cbz:Piyu1cbz@mydatabase.bvhet.mongodb.net/?retryWrites=true&w=majority&appName=MyDatabase"
-
+const Url="mongodb://localhost:27017/"
 const db=mongoose.connect(Url).
 then(()=>{console.log('Connect To Database Success');
 })
 .catch((err)=>{console.log("An Error Occured",err);
 })
-
-
 module.exports=db
